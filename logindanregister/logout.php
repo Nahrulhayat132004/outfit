80978d1bb93a5ea675372a9c1cb2df6f2131e13c
Narl <nahrulaja83@gmail.com>
2025-03-29 10:28:32 +0700

    Delete logindanregister directory

--- logindanregister/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Hapus semua variabel sesi
-session_destroy(); // Hancurkan sesi
-
-// Redirect kembali ke halaman login
-header("Location: login.php");
-exit();
-?>
