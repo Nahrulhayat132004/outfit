7aac0ba465fdcbd583baad2701765b8fdeb59894
Narl <nahrulaja83@gmail.com>
2025-03-29 10:28:23 +0700

    Delete keranjang directory

--- keranjang/localstoragekeranjang.js
+++
@@ -1,7 +0,0 @@
-function addToCart(productName) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push(productName);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    
-    alert(`${productName} berhasil ditambahkan ke keranjang!`);
-}
